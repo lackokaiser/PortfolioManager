b089a44b896f75ce84a3c551a7617b0643ee28d1
Ellie <ellie.cleveland@morganstanley.com>
2025-08-01 11:24:15 +0000

    My javascript and html

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-<script>
+
     fetch('http://localhost:5000/api/data')
     .then(res => res.json())
     .then(data => console.log('GET response:', data));
-</script>
+
+//Helper function to create a table row with cells
+// fucntion createRow(cells) {
+//  const tr = document.creatElement('tr)
+//   cells.forEach(text => {
+//     const td = document.createElement('td); 
+//      td.textContent = text
+//      tr.appendChild(td);
+// });
+// return tr;
+// }
+
+// Fetch and display portfolio data
+// async function loadPortfolio() {
+// try {
+//   const response = await fetch('http://localhost:5000/api/portfolio');
+//   if (!response.ok) throw new Error('Failed to fetch portfolio');
+//   const portfolio = await response.json();
+
+//   const tbody = document.querySelector('portfolio-table tbody');
+//   tbody.inerHTML = '': // clear existing rows
+
+
+// portfolio.forEach(stock => {
+// const gainLoss = ((stock.current_price - stock.avg_buy_price) * stock.shares).toFixed(2);
+// const rows = creatRow([
+//   stock.symbol, 
+//   stock.shares, 
+//   '$${stock.avg_boy_price.}
+//])})
