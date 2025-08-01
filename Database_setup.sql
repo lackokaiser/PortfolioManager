488153ac1b004d1b0bb2e9cdfed010f3615f234f
wisdomokoro1232 <wisdookoro@gmail.com>
2025-08-01 09:46:13 +0000

    DATABASE Entrie

--- Database_setup.sql
+++ Database_setup.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CSFoundations;
 
 use CSFoundations; 
 
-CREATE TABLE stockdemo (
+CREATE TABLE stockdemotwo (
 	transaction_id BIGINT auto_increment PRIMARY KEY NOT NULL,
     ticker VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
