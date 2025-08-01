defb18b923437b5fb083b75df8a26b34035032c7
Laszlo Kaiser <kaiser.lacko@gmail.com>
2025-08-01 09:30:22 +0000

    Cleaned

--- Database_setup.sql
+++ Database_setup.sql
@@ -1,4 +1,3 @@
--- 1. Create a new database named 'myDatabase'
 CREATE DATABASE CSFoundations;
 
 use CSFoundations; 
@@ -11,27 +10,3 @@ CREATE TABLE stockdemo (
     quantity DECIMAL,
     transaction_date DATE DEFAULT (now())
 );
--- 4. Create a 'courses' table with columns: 
--- course_id (numeric, primary key, auto increment), 
--- course_name (string), credit_hours (numeric)
-
--- 5. Add a unique constraint to the course_name column 
--- in the 'courses' table
-
--- 6. Create a 'professors' table with columns: 
--- professor_id (numeric, primary key, auto increment), 
--- first_name (string), last_name (string), subject (string)
-
--- 7. Add a not null constraint to the subject column 
--- in the 'professors' table
-
--- 8. Create a 'classrooms' table with columns: 
--- classroom_id (numeric, primary key, auto increment), 
--- classroom_name (string), capacity (numeric)
-
--- 9. Add a check constraint to the capacity column in 
--- the 'classrooms' table such that capacity should be 
--- greater than 0
-
--- 10. Create a 'enrollment' table to track which student 
--- is taking which course. The table should have the following columns: enrollment_id (numeric, primary key, auto increment), student_id (reference to the students table), course_id (reference to the courses table)
