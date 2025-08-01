a1053d4f03d23c2ecbf1725a846400afa0762e8d
Laszlo Kaiser <kaiser.lacko@gmail.com>
2025-08-01 10:45:56 +0000

    Update to Database schema

--- Database_setup.sql
+++ Database_setup.sql
@@ -3,10 +3,10 @@ CREATE DATABASE CSFoundations;
 use CSFoundations; 
 
 CREATE TABLE stockdemo (
-	transaction_id BIGINT auto_increment PRIMARY KEY NOT NULL,
+	transaction_id BIGINT auto_increment PRIMARY KEY,
     ticker VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    value DECIMAL,
-    quantity DECIMAL,
+    stock_name VARCHAR(50) NOT NULL,
+    stock_value DECIMAL NOT NULL,
+    quantity DECIMAL NOT NULL,
     transaction_date DATE DEFAULT (now())
-);
+    );
