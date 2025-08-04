fd6b2fc6e0dfc9552ae05834dc512d0807433a5d
Doyin Bolaji <d.bolaji0620241@arts.ac.uk>
2025-08-04 01:39:48 +0100

    edits to historical data graph

--- api_history.py
+++ api_history.py
@@ -4,7 +4,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import io
-
+ß
 app = Flask(__name__)
 
 @app.route("/api/v1/stock/<ticker>/history")
@@ -33,34 +33,6 @@ def api_history(ticker):
     plt.close(fig)
     img.seek(0)
     return send_file(img, mimetype='image/png')
-
-
-# def get_history_data(ticker, start, end, interval):
-#     ticker_obj = yf.Tickers(ticker)
-#     data = ticker_obj.history(
-#         start=start,
-#         end=end,
-#         interval=interval
-#     )
-#     return data.reset_index().to_dict(orient="records")
-
-# @app.route("/api/v1/stock/<ticker>/history")
-# def api_history(ticker):
-#     today = datetime.today()
-#     default_start = (today - timedelta(days=365)).strftime("%Y-%m-%d")
-#     default_end = today.strftime("%Y-%m-%d")
-#     default_interval = "1d"
-
-#     # Use defaults if not provided
-#     start = request.args.get("start", default_start)
-#     end = request.args.get("end", default_end)
-#     interval = request.args.get("interval", default_interval)
-
-#     data_json = get_history_data(ticker, start, end, interval)
-#     plt.plot(data_json["History"])
-#     return jsonify({"ticker": ticker, "history": data_json})
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
