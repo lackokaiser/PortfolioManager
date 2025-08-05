034d69ea930e4578a930de4fe745a4d5f798a2ec
Laszlo Kaiser <kaiser.lacko@gmail.com>
2025-08-05 11:34:12 +0000

    Change to schema

--- Database_setup.sql
+++ Database_setup.sql
@@ -6,7 +6,7 @@ CREATE TABLE stockdemo (
 	transaction_id BIGINT auto_increment PRIMARY KEY,
     ticker VARCHAR(50) NOT NULL,
     stock_name VARCHAR(50) NOT NULL,
-    stock_value DECIMAL NOT NULL,
-    quantity DECIMAL NOT NULL,
+    stock_value float NOT NULL,
+    quantity float NOT NULL,
     transaction_date DATE DEFAULT (now())
     );
